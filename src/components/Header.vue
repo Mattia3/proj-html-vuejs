b2100ff9282764c19156f260c7aead1cf8656311
Mattia3 <mcortinovis9@gmail.com>
2021-11-22 19:34:20 +0100

    Created/Updated build

---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+  <header class="header-container pb-5">
+    <button class="button-left">PREV</button>
+    <button class="button-right">NEXT</button>
+    <!-- Inizio Navbar -->
+    <div class="container d-flex justify-content-between align-items-center">
+      <ul>
+        <li><button class="header-button">ORDER ONLINE</button></li>
+      </ul>
+
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">PAGES</a></li>
+        <li><a href="#">MENU</a></li>
+        <li>
+          <img
+            src="@/assets/img/h5-logo-divided-header.png"
+            alt="Logo Navbar"
+          />
+        </li>
+        <li><a href="#">EVENT</a></li>
+        <li><a href="#">BLOG</a></li>
+        <li><a href="#">LANDING</a></li>
+      </ul>
+
+      <ul>
+        <li><a href="#">CART</a></li>
+        <li><a href="#">SEARCH</a></li>
+      </ul>
+    </div>
+    <!-- Fine Navbar -->
+    <!-- Inizio Hero -->
+    <div class="container background-header-img">
+      <img
+        class="header-img-absolute"
+        src="@/assets/img/h3-rev-img-6.png"
+        alt=""
+      />
+    </div>
+    <!-- Fine Hero -->
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+@import "../style/variables";
+
+.header-container {
+  background-color: black;
+  padding: $padding;
+  position: relative;
+
+  .header-button {
+    background-color: orangered;
+    border: none;
+    padding: 10px 20px;
+    color: $color_white;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+
+    li {
+      display: flex;
+      align-self: center;
+
+      img {
+        width: 140px;
+      }
+    }
+
+    a {
+      text-decoration: none;
+      color: $color_white;
+      padding: 0 20px;
+    }
+  }
+
+  .background-header-img {
+    background-image: url(../assets/img/h3-rev-img-5.png);
+    text-align: center;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,352 @@
+<template>
+  <main>
+    <!-- Inizio prima section -->
+    <div class="row-cols-4 d-flex">
+      <div class="col">
+        <img class="main-img-section" src="@/assets/img/h3-img-1.jpg" alt="" />
+      </div>
+      <div class="col">
+        <img class="main-img-section" src="@/assets/img/h3-img-2.jpg" alt="" />
+      </div>
+      <div class="col">
+        <img class="main-img-section" src="@/assets/img/h3-img-3.jpg" alt="" />
+      </div>
+      <div class="col">
+        <img class="main-img-section" src="@/assets/img/h3-img-4.jpg" alt="" />
+      </div>
+    </div>
+    <!-- Fine prima section -->
+
+    <!-- Inizio Seconda Section -->
+    <div class="background-image-hello">
+      <button class="button-left">PREV</button>
+      <button class="button-right">NEXT</button>
+
+      <div class="container container-hello">
+        <div class="row justify-content-center">
+          <div class="col-9">
+            <img src="@/assets/svg/svg-11.svg" alt="" />
+            <h4 class="fw-bold">
+              "FORGET THE TRENDY PIZZA SHOPS, THIS HIDDEN SPOT MAKES THE BEST
+              NEW YORK-STYLE PIZZA SLICE IN NAPLES"
+            </h4>
+            <p class="text-color">WASHINGTON POST 2018</p>
+          </div>
+        </div>
+      </div>
+      <img class="pizza-svg" src="@/assets/svg/svg-4.svg" alt="" />
+    </div>
+    <!-- Fine Seconda Section -->
+
+    <!-- Inizio Special Section -->
+    <div class="row py-2">
+      <div class="col-6">
+        <img class="img-special" src="@/assets/img/h1-img-4.jpg" alt="" />
+      </div>
+
+      <div class="col-6 container-special position-relative">
+        <div class="col-5">
+          <h3 class="fw-bold">SPECIALS*</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectur adipiscing elit. Praesent ut.
+          </p>
+          <div class="d-flex">
+            <h4 class="pe-3 text-danger fw-bold">$10</h4>
+            <div>
+              <h4 class="fw-bold">COMBO MEZZO</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectur adipiscing elit. Praesent
+                ut.
+              </p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <h4 class="pe-3 text-danger fw-bold">$20</h4>
+            <div>
+              <h4 class="fw-bold">COMBO MEZZO</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectur adipiscing elit. Praesent
+                ut.
+              </p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <h4 class="pe-3 text-danger fw-bold">$30</h4>
+            <div>
+              <h4 class="fw-bold">COMBO GRANDE</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectur adipiscing elit. Praesent
+                ut.
+              </p>
+            </div>
+          </div>
+        </div>
+        <img class="svg-pizza-special" src="@/assets/svg/svg-4.svg" alt="" />
+      </div>
+    </div>
+    <!-- Fine Special Section -->
+    <!-- Inizio judge section -->
+    <div class="background-img-judge">
+      <div class="row">
+        <div class="col-4 container">
+          <h2>GO AHEAD AND BUILD YOUR OWN PIZZA WE WON'T JUDGE!</h2>
+        </div>
+      </div>
+    </div>
+    <!-- Fine judge section -->
+    <!-- Inizio section people -->
+    <div class="row row-cols-4">
+      <div class="col p-0">
+        <img
+          class="img-people"
+          src="@/assets/img/h1-team-1a-700x700.jpg"
+          alt=""
+        />
+      </div>
+      <div class="col p-0">
+        <img class="img-people" src="@/assets/img/h1-team-2a.jpg" alt="" />
+      </div>
+      <div class="col p-0">
+        <img class="img-people" src="@/assets/img/h1-team-4a.jpg" alt="" />
+      </div>
+      <div class="col p-0">
+        <img class="img-people" src="@/assets/img/h1-team-3a.jpg" alt="" />
+      </div>
+    </div>
+    <!-- Fine section people -->
+    <!-- Inizio section loghi -->
+    <div class="row container-loghi">
+      <div class="col-12 d-flex justify-content-center py-5">
+        <img src="@/assets/img/h1-clients-img-4.png" alt="" />
+        <img src="@/assets/img/h1-clients-img-3.png" alt="" />
+        <img src="@/assets/img/h1-clients-img-1.png" alt="" />
+        <img src="@/assets/img/h1-clients-img-2.png" alt="" />
+        <img src="@/assets/img/h1-clients-img-5.png" alt="" />
+      </div>
+    </div>
+    <!-- Fine section loghi -->
+    <!-- Inzio pizze -->
+    <div class="container py-5 text-center">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <h6 class="text-danger">CHOSE YOUR FLAVOR</h6>
+          <h2 class="fw-bold">THE BEST PIZZA MENU IN TOWN</h2>
+
+          <p class="text-muted">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            inventore et laboriosam? Beatae dolor asperiores aliquid, illum
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row row-cols-6 text-center pb-5">
+      <div class="col">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-1a-100x100.png"
+          alt="PIZZA BISMARK"
+        />
+        <h6 class="title-oro fw-bold pt-3">BISMARK</h6>
+        <span class="text-muted text-decoration-line-through">$5.00</span>
+        <span class="title-orange px-2">$30.00</span>
+      </div>
+      <div class="col">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-2a-150x150.png"
+          alt="PIZZA FIORI DI ZUCCA"
+        />
+        <h6 class="title-oro fw-bold pt-3">FIORI DI ZUCCA</h6>
+        <span class="title-orange px-2">$7.00 - $50.00</span>
+      </div>
+      <div class="col position-relative">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-6a-100x100.png"
+          alt="PIZZA VALDOSTANA"
+        />
+        <h6 class="title-oro fw-bold pt-3">VALDOSTANA</h6>
+        <span class="title-orange px-2">$55.00</span>
+        <p class="soldout-pizza">SOLD</p>
+      </div>
+      <div class="col">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-4a-150x150.png"
+          alt="PIZZA PIZZA TARTUFATA"
+        />
+        <h6 class="title-oro fw-bold pt-3">PIZZA TARTUFATA</h6>
+        <span class="title-orange px-2">$45.00</span>
+      </div>
+      <div class="col">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-5a-150x150.png"
+          alt="PIZZA FRANCESCANA"
+        />
+        <h6 class="title-oro fw-bold pt-3">FRANCESCANA</h6>
+        <span class="title-orange px-2">$25.00</span>
+      </div>
+      <div class="col">
+        <img
+          class="img-pizze"
+          src="@/assets/img/h3-product-img-3a-150x150.png"
+          alt="PIZZA CAMPAGNOLA"
+        />
+        <h6 class="title-oro fw-bold pt-3">CAMPAGNOLA</h6>
+        <span class="title-orange px-2">$50 - $95.00</span>
+      </div>
+    </div>
+    <!-- Fine pizze -->
+
+    <div class="row">
+      <div
+        class="
+          col-6
+          d-flex
+          justify-content-center
+          align-items-center
+          text-white
+          background-date
+        "
+      >
+        <div class="col-6">
+          <h6 class="title-orange">MADE WITH LOVE</h6>
+          <h3 class="pb-4">DELISH PIZZA DEALS</h3>
+          <div class="d-flex text-date">
+            <div class="pe-4">
+              <h3 class="title-orange">02</h3>
+              <h6>NOV</h6>
+            </div>
+            <div>
+              <h3>TRADITIONAL NEAPOLTINA PIES IN KYOTO PIZZA MERCATO</h3>
+              <p class="title-orange">204 E. Pizzetta Tommaso</p>
+            </div>
+          </div>
+          <div class="d-flex text-date py-4">
+            <div class="pe-4">
+              <h3 class="title-orange">02</h3>
+              <h6>NOV</h6>
+            </div>
+            <div>
+              <h3>TRADITIONAL NEAPOLTINA PIES IN KYOTO PIZZA MERCATO</h3>
+              <p class="title-orange">204 E. Pizzetta Tommaso</p>
+            </div>
+          </div>
+          <div class="d-flex py-4">
+            <div class="pe-4">
+              <h3 class="title-orange">02</h3>
+              <h6>NOV</h6>
+            </div>
+            <div>
+              <h3>TRADITIONAL NEAPOLTINA PIES IN KYOTO PIZZA MERCATO</h3>
+              <p class="title-orange">204 E. Pizzetta Tommaso</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6">
+        <img src="@/assets/img/h3-img-5a.jpg" alt="" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss">
+@import "../style/variables";
+.main-img-section {
+  width: 100%;
+  border: 7px solid $color_white;
+}
+
+.background-image-hello {
+  background-image: url(../assets/img/h3-testimonials-bckgrnd.jpg);
+  background-position: center;
+  position: relative;
+  padding: $padding;
+
+  .container-hello {
+    padding: $padding_large;
+    text-align: center;
+  }
+
+  .text-color {
+    color: $color_text;
+  }
+
+  .pizza-svg {
+    position: absolute;
+    right: 70px;
+    bottom: 30px;
+  }
+}
+
+.container-special {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .svg-pizza-special {
+    position: absolute;
+    right: 40px;
+    bottom: 200px;
+    border-radius: 50%;
+    background: rgba(246, 247, 242, 255);
+    padding: 10px 10px;
+  }
+}
+
+.img-special {
+  width: 100%;
+}
+
+.background-img-judge {
+  background-image: url(../assets/img/h3-background-img.jpg);
+  width: 100%;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  color: $color_white;
+}
+
+.img-people {
+  width: 100%;
+  padding: 10px 0;
+}
+
+.container-loghi {
+  background-color: rgba(247, 247, 242, 255);
+}
+
+.img-pizze {
+  width: 70%;
+}
+
+.soldout-pizza {
+  position: absolute;
+  right: 63px;
+  top: 14px;
+  background: $color_text;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  color: $color_white;
+  line-height: 60px;
+}
+
+.background-date {
+  background-color: black;
+
+  .text-date {
+    border-bottom: 1px dotted;
+  }
+}
+</style>
